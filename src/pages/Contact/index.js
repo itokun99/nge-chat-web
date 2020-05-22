e4c576e8fe7d3911e94d265ee2245a241e6d6f80
Indrawan Lisanto <indrawan@aleph-labs.com>
2020-05-22 14:08:04 +0700

    add skeleton loader

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -15,34 +15,81 @@ import {
   FormGroup,
   FormText,
   ChatItem,
-  Container
+  Container,
+  Skeleton
 } from 'components';
-import { getUser, getContacts } from 'services';
+import { getUser, getContacts, showPopup } from 'services';
+import { createMessageFirebase } from 'utils';
 
 const Contact = ({ users }) => {
+  const [loading, setLoading] = React.useState(true);
   const history = useHistory();
 
   const navigateToDetail = userId => {
     history.push(`/user/${userId}`);
   };
 
-  const renderList = () => (
-    <React.Fragment>
-      {users.map((user, index) => (
-        <ChatItem
-          key={index}
-          onClick={() => navigateToDetail(user.userId)}
-          title={user.name}
-          description={user.email}
-          image={user.photo}
-        />
-      ))}
-    </React.Fragment>
-  );
+  const initData = React.useCallback(async () => {
+    try {
+      setLoading(true);
+      await getContacts();
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      showPopup({
+        title: 'Terjadi Kesalahan!',
+        description: err.code
+          ? createMessageFirebase(err.code)
+          : createMessageFirebase(),
+        onClickButton: initData
+      });
+      throw err;
+    }
+  }, []);
+
+  const renderList = () => {
+    if (loading) {
+      return [1, 2, 3].map(val => (
+        <div
+          key={val}
+          style={{
+            padding: '0 24px',
+            display: 'flex',
+            alignItems: 'center',
+            marginBottom: 24
+          }}
+        >
+          <Skeleton radius={8} width={50} circle style={{ marginRight: 16 }} />
+          <div>
+            <Skeleton
+              radius={8}
+              width={200}
+              height={18}
+              style={{ marginBottom: 8 }}
+            />
+            <Skeleton radius={8} width={100} height={12} />
+          </div>
+        </div>
+      ));
+    }
+    return (
+      <React.Fragment>
+        {users.map((user, index) => (
+          <ChatItem
+            key={index}
+            onClick={() => navigateToDetail(user.userId)}
+            title={user.name}
+            description={user.email}
+            image={user.photo}
+          />
+        ))}
+      </React.Fragment>
+    );
+  };
 
   React.useEffect(() => {
-    getContacts();
-  }, []);
+    initData();
+  }, [initData]);
 
   return (
     <PrivateContainer>
