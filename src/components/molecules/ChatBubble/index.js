24b8dbb7f53eef056a431e95fca8a2a682873ab4
Indrawan Lisanto <indrawan@aleph-labs.com>
2020-05-24 04:07:20 +0700

    add date for message

--- src/components/molecules/ChatBubble/index.js
+++ src/components/molecules/ChatBubble/index.js
@@ -1,25 +1,30 @@
 import { React, PropTypes, cx } from 'libraries';
 
-const ChatBubble = ({ content, align }) => {
+const ChatBubble = ({ content, align, date }) => {
   const wrapperClass = cx('ChatBubble', {
     [`ChatBubble--${align}`]: align
   });
 
   return (
     <div className={wrapperClass}>
-      <div className="ChatBubble__content">{content}</div>
+      <div className="ChatBubble__content">
+        <div className="ChatBubble__message">{content}</div>
+        <div className="ChatBubble__meta">{date}</div>
+      </div>
     </div>
   );
 };
 
 ChatBubble.propTypes = {
   content: PropTypes.string,
-  align: PropTypes.oneOf(['left', 'right'])
+  align: PropTypes.oneOf(['left', 'right']),
+  date: PropTypes.string
 };
 
 ChatBubble.defaultProps = {
   content: '',
-  align: 'left'
+  align: 'left',
+  date: null
 };
 
 export default ChatBubble;
